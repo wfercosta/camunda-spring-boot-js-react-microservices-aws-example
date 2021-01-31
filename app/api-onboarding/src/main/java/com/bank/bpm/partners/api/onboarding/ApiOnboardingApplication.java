bc983c3bc79b7aa6fd73b1da67d47962d7dcb57d
Wander Costa <wanderfernando@gmail.com>
2021-01-31 10:41:35 -0300

    Adds initial project structure

---
+++ app/api-onboarding/src/main/java/com/bank/bpm/partners/api/onboarding/ApiOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.bank.bpm.partners.api.onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOnboardingApplication.class, args);
+	}
+
+}
---
+++ app/api-onboarding/src/test/java/com/bank/bpm/partners/api/onboarding/ApiOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.bpm.partners.api.onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

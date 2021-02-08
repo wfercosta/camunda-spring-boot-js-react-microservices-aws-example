4403e371253c5dad037ff286c0fff60e5ee8050a
Wander Costa <wanderfernando@gmail.com>
2021-02-08 11:08:32 -0300

    Adds removal of component to ms-registration

--- app/spring-starters/external-task-spring-boot-autoconfigure/src/main/java/com/bank/bpm/partners/onboarding/starters/ExternalTaskAutoConfiguration.java
+++ app/spring-starters/external-task-spring-boot-autoconfigure/src/main/java/com/bank/bpm/partners/onboarding/starters/ExternalTaskAutoConfiguration.java
@@ -2,6 +2,8 @@ package com.bank.bpm.partners.onboarding.starters;
 
 import com.bank.bpm.partners.workers.onboarding.component.ExternalTaskManager;
 import org.camunda.bpm.client.task.ExternalTaskHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
@@ -16,8 +18,12 @@ import java.util.stream.Collectors;
 @EnableConfigurationProperties(ExternalTasksProperties.class)
 public class ExternalTaskAutoConfiguration {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ExternalTaskAutoConfiguration.class);
+
 	@Bean
 	public ExternalTaskManager externalTaskManager(final ExternalTasksProperties properties, final ApplicationContext context) {
+
+		LOG.info("-> Auto configuration initialization");
 
 		Map<String, ExternalTaskHandler> beans = context.getBeansOfType(ExternalTaskHandler.class);
 

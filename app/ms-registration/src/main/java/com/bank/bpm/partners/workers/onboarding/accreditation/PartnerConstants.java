f2cb86dbaf97ff9fb66815504cf514168c2a6177
Wander Costa <wanderfernando@gmail.com>
2021-03-21 15:55:48 -0300

    Deletes unused files in project

--- app/ms-registration/src/main/java/com/bank/bpm/partners/workers/onboarding/accreditation/PartnerConstants.java
+++
@@ -1,9 +0,0 @@
-package com.bank.bpm.partners.workers.onboarding.accreditation;
-
-public interface PartnerConstants {
-
-	String ERROR_PARTNER_REGISTRATION_DUPLICATED = "partner_duplication";
-	String VARIABLE_BODY = "body";
-	String VARIABLE_MLP_RESULTS = "mlp_results";
-	String VARIABLE_MLP_STATUS = "mlp_status";
-}
--- app/ms-registration/src/main/java/com/bank/bpm/partners/workers/onboarding/shared/MoneyLaunderingStatusType.java
+++
@@ -1,5 +0,0 @@
-package com.bank.bpm.partners.workers.onboarding.shared;
-
-public enum MoneyLaunderingStatusType {
-	SUITABLE, UNFAITHFUL, INCONCLUSIVE;
-}
